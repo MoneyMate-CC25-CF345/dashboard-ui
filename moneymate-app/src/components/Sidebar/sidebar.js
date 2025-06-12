5c21f9d859ca0471f28de0378ef0c6cfedaff5a4
Obiwannn11 <118538946+Obiwannn11@users.noreply.github.com>
2025-06-13 03:29:37 +0800

    memperbaiki sidebar untuk memulai proses routig dengan href #

--- moneymate-app/src/components/Sidebar/sidebar.js
+++ moneymate-app/src/components/Sidebar/sidebar.js
@@ -1,21 +1,21 @@
-// Impor file CSS yang spesifik untuk komponen ini
 import './Sidebar.css';
 
-// Fungsi ini akan dipanggil untuk membuat HTML dari sidebar
 export function createSidebar() {
-  // Kita gunakan backtick (`) untuk membuat string multi-baris
   return `
     <nav class="sidebar">
       <div class="sidebar-header">
         <img src="/big-logo.png" alt="MoneyMate Logo" class="logo-big">
         <img src="/mini-logo.png" alt="MoneyMate Mini Logo" class="logo-mini">
       </div>
-      <ul class="sidebar-menu">
-        <li><a href="#" class="active"><i class="fa-solid fa-book-bookmark"></i> <span>Buku Catatan</span></a></li>
-        <li><a href="#"><i class="fa-solid fa-wallet"></i> <span>Dompet</span></a></li>
-        <li><a href="#"><i class="fa-solid fa-file-alt"></i> <span>Laporan</span></a></li>
-        <li><a href="#"><i class="fa-solid fa-plus-circle"></i> <span>Lainnya</span></a></li>
-        <li><a href="#"><i class="fa-solid fa-cog"></i> <span>Pengaturan</span></a></li>
+      
+      <ul class="sidebar-menu" id="sidebar-menu"> 
+        
+        <li><a href="#/"><i class="fa-solid fa-book-bookmark"></i> <span>Buku Catatan</span></a></li>
+        <li><a href="#/dompet"><i class="fa-solid fa-wallet"></i> <span>Dompet</span></a></li>
+        <li><a href="#/laporan"><i class="fa-solid fa-file-alt"></i> <span>Laporan</span></a></li>
+        <li><a href="#/lainnya"><i class="fa-solid fa-plus-circle"></i> <span>Lainnya</span></a></li>
+        <li><a href="#/pengaturan"><i class="fa-solid fa-cog"></i> <span>Pengaturan</span></a></li>
+
       </ul>
       <div class="sidebar-footer">
         <button class="logout-btn">
