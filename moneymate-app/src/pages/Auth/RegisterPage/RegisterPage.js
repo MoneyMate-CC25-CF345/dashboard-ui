6c074a403fe68591d57cd23f268cc11a295cd6b5
Obiwannn11 <118538946+Obiwannn11@users.noreply.github.com>
2025-06-13 16:34:28 +0800

    membuat register page, dan menyesuaiikan dengan layout khussus regisyer yaitu 75 25

--- moneymate-app/src/pages/Auth/RegisterPage/RegisterPage.js
+++ moneymate-app/src/pages/Auth/RegisterPage/RegisterPage.js
@@ -3,9 +3,45 @@ import './RegisterPage.css';
 
 export function createRegisterPage() {
   return `
-    <div>
-      <h1 style="color:white; text-align:center;">Halaman Registrasi</h1>
-      <p style="color:white; text-align:center;">Layout 70/30 berfungsi bismillah</p>
+    <div class="register-content-wrapper">
+      
+      <div class="register-form-container">
+        <form class="register-form">
+          <h2>Buat Akun</h2>
+          <div class="input-group">
+            <i class="fa-solid fa-user"></i>
+            <input type="text" placeholder="Nama Lengkap" required>
+          </div>
+          <div class="input-group">
+            <i class="fa-solid fa-envelope"></i>
+            <input type="email" placeholder="Email" required>
+          </div>
+          <div class="input-group">
+            <i class="fa-solid fa-lock"></i>
+            <input type="password" placeholder="Kata Sandi" required>
+          </div>
+          <div class="input-group">
+            <i class="fa-solid fa-lock"></i>
+            <input type="password" placeholder="Konfirmasi Kata Sandi" required>
+          </div>
+           <div class="input-group">
+            <i class="fa-solid fa-phone"></i>
+            <input type="tel" placeholder="Nomor Ponsel" required>
+          </div>
+          <div class="terms-container">
+            <input type="checkbox" id="terms" name="terms" required>
+            <label for="terms">Dengan ini saya setuju syarat dan ketentuan yang berlaku</label>
+          </div>
+        </form>
+      </div>
+
+      <div class="register-actions-container">
+        <button type="submit" class="btn btn-register">Mendaftar</button>
+        <p class="login-link">
+          Sudah memiliki akun? <a href="#/login">Masuk</a>
+        </p>
+      </div>
+
     </div>
   `;
 }
